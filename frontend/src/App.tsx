d7bce9171c94051d4265975c40f9649e790a27c5
eduardonsm <eduardo.nogueira.severo.macedo@ccc.ufcg.edu.br>
2026-03-24 21:29:43 -0300

    feat: implement authentication redirection and create Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 import RegisterEmployee from "./pages/RegisterEmployee";
 import CreateOrder from "./pages/CreateOrder";
 
@@ -13,11 +15,34 @@ function OrdersPage() {
   );
 }
 
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  if (!localStorage.getItem("token")) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
         <Route path="/pedidos" element={<OrdersPage />} />
         <Route path="/pedidos/novo" element={<CreateOrder />} />
         <Route path="/cadastrar-funcionario" element={<RegisterEmployee />} />
